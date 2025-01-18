4303e0c8f5ff82928ab039e94f37045cb5d27069
Honor91 <acampos91c@gmail.com>
2025-01-18 14:42:20 -0500

    agregando cosas

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -6,3 +6,21 @@ console.log(numeroUsuario);
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
 }
+
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+let nombre = 'Lua';
+let edad = 25;
+parseInt(edad);
+let numeroDeVentas = 50;
+parseInt(numeroDeVentas);
+let saldoDisponible = 1000;
+let mensajeDeError ="¡Error! Completa todos los campos";
+alert(mensajeDeError);
+nombre = prompt("Ingrese su nombre");
+edad = prompt("ingrese su edad");
+
+if ( edad >= 18 ) {
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
