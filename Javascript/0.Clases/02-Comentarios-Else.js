edcbe36993b7923e34b2725c6b90502efeb83b45
Honor91 <acampos91c@gmail.com>
2025-01-18 22:59:27 -0500

    archivos hasta desafio2

---
+++ Javascript/0.Clases/02-Comentarios-Else.js
@@ -0,0 +1,20 @@
+alert('Hola mundo')
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor");
+
+console.log(numeroUsuario);
+/* 
+
+A comentar en bloque
+y en varias lineas
+*/
+
+// Segunda forma para comentar en linea
+
+if ( numeroUsuario == numeroSecreto ) {
+    alert(`Acertaste el numero secreto es: ${numeroSecreto}`)
+} else {
+    alert("Lo siento, no acertaste el numero")
+    console.log('holi')
+}
+// sorry por las tildes estoy usando teclado americano no tiene tilde, me es mas comodo.6
