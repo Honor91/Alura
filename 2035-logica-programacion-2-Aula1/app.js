984fdfc68f2da3bccf3097f75adf5e2d855b1e24
Honor91 <acampos91c@gmail.com>
2025-01-21 00:21:08 -0500

    nuevo

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,11 +1,26 @@
+let numeroSecreto = generarNumeroSecreto()
 
 function asignarTextoElemento(elemento,texto) {
     let titulo = document.querySelector(elemento);
     titulo.innerHTML = texto;
 }
 
+function verificarIntento() {
+    let numeroDelUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroSecreto);
+    console.log(numeroDelUsuario);
+    console.log(numeroDelUsuario == numeroSecreto);
+}
+
 function intentoDeUsuario() {
     alert('click desde la funcion');
 }
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+}
+
 asignarTextoElemento('h1','Juego del numero secreto');
 asignarTextoElemento('p','Indica un numero del 1 al 10');
+verificarIntento();
