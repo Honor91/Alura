dac1cbbab901250578ae96353f052835384f6b54
Honor91 <acampos91c@gmail.com>
2025-01-22 20:18:59 -0500

    agregando archivos para desafio

--- 2035-logica-programacion-2-Aula2/app.js
+++ 2035-logica-programacion-2-Aula2/app.js
@@ -1,7 +1,6 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
 let intentos = 0;
 
-console.log(numeroSecreto);
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -10,15 +9,19 @@ function asignarTextoElemento(elemento, texto) {
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(`intentos: ${intentos}`);
     
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p','Acertaste el numero');
+        asignarTextoElemento('p',`Usaste ${intentos} ${intentos == 1? 'intento': 'intentos'} para acertar al numero secreto`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p','El numero secreto es nenor');
         } else{
             asignarTextoElemento('p','El numero secreto es mayor');
         }
+        intentos++;
+        limpiarCaja();
     }
     return;
 }
@@ -29,5 +32,27 @@ function generarNumeroSecreto() {
 
 }
 
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p',`Indica un número del 1 al 10`);
+function limpiarCaja() {
+    let valorCaja = document.querySelector('#valorUsuario').value = "";
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al 10`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+function reiniciarJuego(){
+    // Necesitamos limpiar el input
+    limpiarCaja();
+    // Indicar mensaje de inicio
+    // Generar el numero aleatorio
+    
+    // Inicializar el numero de intentos
+    condicionesIniciales()
+    // Deshabilitar el boton de "Nuevo Juego"
+    document.querySelector('#reiniciar').setAttribute('disabled','True');
+    console.log(numeroSecreto);
+}
+condicionesIniciales();
+reiniciarJuego();
